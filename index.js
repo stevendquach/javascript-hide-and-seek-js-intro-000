823c0205cc49d807d6c06d0605a7df7694acff41
Steven Quach <stevendquach@gmail.com>
2020-10-06 09:34:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+function increaseRankBy(n) {
+    var ranks = document.querySelectorAll('ul.ranked-list li');
+    for (let i = 0; i<ranks.length; i++){
+      ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+    }
+}
